3723535fcf2f24eecaeb6fae6449d1a558577187
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2024-04-13 22:06:51 +0200

    setup app theme

--- CryptoTracker/Extensions/Color.swift
+++ CryptoTracker/Extensions/Color.swift
@@ -5,4 +5,16 @@
 //  Created by Abanoub Ashraf on 13/04/2024.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}

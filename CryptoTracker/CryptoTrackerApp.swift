3355d56b8db3edfde00dc7f586b33165152c3b77
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2024-04-13 21:43:06 +0200

    prepare the app

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Abanoub Ashraf on 13/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

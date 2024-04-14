dc893cd4a8255c316a441b587fcd73b3a99861e8
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2024-04-14 21:15:55 +0200

    make circle button view with animation

---
+++ CryptoTracker/Core/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,41 @@
+//
+//  CircleButtonView.swift
+//  CryptoTracker
+//
+//  Created by Abanoub Ashraf on 13/04/2024.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    let iconName: String
+    
+    var body: some View {
+        Image(systemName: iconName)
+            .font(.headline)
+            .foregroundColor(Color.theme.accent)
+            .frame(width: 50, height: 50)
+            .background(
+                Circle()
+                    .foregroundColor(Color.theme.background)
+            )
+            .shadow(
+                color: Color.theme.accent.opacity(0.25), 
+                radius: 10,
+                x: 0,
+                y: 0
+            )
+            .padding()
+    }
+}
+
+#Preview {
+    Group {
+        CircleButtonView(iconName: "info")
+            .previewLayout(.sizeThatFits)
+        
+        CircleButtonView(iconName: "plus")
+            .previewLayout(.sizeThatFits)
+            .colorScheme(.dark)
+    }
+}

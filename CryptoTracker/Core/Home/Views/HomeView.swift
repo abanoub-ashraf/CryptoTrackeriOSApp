813398af5a3349d88adfba08121b144c293a8fed
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2024-04-14 21:18:10 +0200

    home view header section

---
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -0,0 +1,63 @@
+//
+//  HomeView.swift
+//  CryptoTracker
+//
+//  Created by Abanoub Ashraf on 13/04/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                homeHeader
+                
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            
+            Spacer()
+            
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            
+            Spacer()
+            
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    NavigationView {
+        HomeView()
+            .navigationBarHidden(true)
+    }
+}

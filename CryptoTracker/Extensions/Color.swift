5c3e11037056d54184a96050e3781af7e828dc69
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2024-04-15 20:01:50 +0200

    useful extensions

--- CryptoTracker/Extensions/Color.swift
+++ CryptoTracker/Extensions/Color.swift
@@ -14,7 +14,7 @@ extension Color {
 struct ColorTheme {
     let accent = Color("AccentColor")
     let background = Color("BackgroundColor")
-    let green = Color("GreenColor")
-    let red = Color("RedColor")
+    let green = Color("CustomGreenColor")
+    let red = Color("CustomRedColor")
     let secondaryText = Color("SecondaryTextColor")
 }

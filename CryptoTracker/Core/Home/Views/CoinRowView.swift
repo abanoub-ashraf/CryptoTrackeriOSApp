9a9b6cf53efc3f49c3d77a6f9927b5dce02caa3c
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2024-04-15 20:17:50 +0200

    coin row view

---
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,82 @@
+//
+//  CoinRowView.swift
+//  CryptoTracker
+//
+//  Created by Abanoub Ashraf on 14/04/2024.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: CoinModel
+    let showHoldingColumn: Bool
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            leftColumn
+            
+            Spacer()
+            
+            if showHoldingColumn {
+                centerColumn
+            }
+            
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+extension CoinRowView {
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            
+            Circle()
+                .frame(width: 30, height: 30)
+            
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundColor(Color.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ? Color.theme.green : Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview(traits: .sizeThatFitsLayout) {
+    CoinRowView(coin: Preview.dev.coin, showHoldingColumn: true)
+}
+
+@available(iOS 17.0, *)
+#Preview(traits: .sizeThatFitsLayout) {
+    CoinRowView(coin: Preview.dev.coin, showHoldingColumn: true)
+        .preferredColorScheme(.dark)
+}

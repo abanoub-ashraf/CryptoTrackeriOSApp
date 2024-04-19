ac9be633e8e9288a4ee454f3c56e13a800d3454c
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2024-04-19 19:30:31 +0200

    creating a reusable network layer

---
+++ CryptoTracker/Utilities/NetworkingManager.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkingManager.swift
+//  CryptoTracker
+//
+//  Created by Abanoub Ashraf on 19/04/2024.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager {
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+                case .badURLResponse(url: let url):
+                    return "[🔥] Bad response from URL: \(url)"
+                case .unknown:
+                    return "[⚠️] Unknown error occurred"
+            }
+        }
+    }
+    
+    static func download(url: URL) -> AnyPublisher<Data, any Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap ({ try handleUrlResponse(output: $0, url: url) })
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    static func handleUrlResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
+        
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<any Publishers.Decode<AnyPublisher<Data, any Error>, [CoinModel], JSONDecoder>.Failure>) {
+        switch completion {
+            case .finished:
+                break
+            case .failure(let error):
+                print(error.localizedDescription)
+        }
+    }
+}

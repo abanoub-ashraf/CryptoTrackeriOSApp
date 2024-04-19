e0418d905f7f2292d86f6dd3d1f11681a8914095
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2024-04-19 21:10:29 +0200

    cache downloaded images and get them from cach if exists

--- CryptoTracker/Services/CoinImageService.swift
+++ CryptoTracker/Services/CoinImageService.swift
@@ -13,22 +13,37 @@ class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         self.getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            self.image = savedImage
+        } else {
+            self.downloadCoinImage()
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else { return }
         
-        imageSubscription = NetworkingManager.download(url: url)
+        imageSubscription = NetworkManager.download(url: url)
             .tryMap { data -> UIImage? in
                 return UIImage(data: data)
             }
-            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+            .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] returnedImage in
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
